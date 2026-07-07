9f097ff3a164c3427ad191c95cfe2e8f07c0c63b
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-07-07 11:34:55 +0530

    connection section completed

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -2,8 +2,11 @@ import { BadgeCheck, Heart, MessageCircle, Share2 } from 'lucide-react'
 import React,{ useState} from 'react'
 import moment from 'moment'
 import { dummyUserData } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const PostCard = ({post}) => {
+
+    const navigate = useNavigate();
 
     const postWithHashtags = post.content.replace(/(#\w+)/g, '<span class="text-indigo-500">$1</span>');
     const [likes, setLikes] = React.useState(post.likes_count);
@@ -16,7 +19,7 @@ const PostCard = ({post}) => {
   return (
     <div className=" bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl">
         {/* User Info */}
-        <div className="inline-flex items-center gap-3 cursor-pointer">
+        <div onClick={()=> navigate("/profile/"+ post.user._id)} className="inline-flex items-center gap-3 cursor-pointer">
             <img src={post.user.profile_picture} alt="" className="w-10 h-10 rounded-full shadow"  />
             <div>
                 <div className="flex items-center space-x-1">
--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { dummyConnectionsData } from '../assets/assets'
+import { Eye, MessageSquare } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const Messages = () => {
+
+  const naviagete = useNavigate();
+
   return (
-    <div>
-      
+    <div className="min-h-screen ml-70 relative bg-slate-50">
+      <div className="max-w-6xl mx-auto p-6">
+          {/* Title */}
+          <div className="mb-8">
+            <h1 className="text-3xl font-bold text-slate-900 mb-2">Messages</h1>
+            <p className="text-slate-600"> Talk to your friends and connections</p>
+          </div>
+          {/* Connected Users */}
+          <div className="flex flex-col gap-3">
+            {dummyConnectionsData.map((user)=>(
+              <div key = {user._id} className="max-w-xl flex flex-warp gap-5 p-6 bg-white shadow rounded-md">
+                <img src={user.profile_picture} alt="" className="size-12 mx-auto rounded-full" />
+                <div className="flex-1">
+                  <p className="font-medium text-slate-700">{user.full_name}</p>
+                  <p className=" text-slate-500">@{user.username}</p>
+                  <p className=" text-sm text-gray-600">{user.bio}</p>
+                </div>
+                <div className="flex flex-col gap-2 mt-4">
+                      <button onClick={()=>naviagete(`/messages/${user._id}`)} className="size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cirsor-pointer gap-1">
+                        <MessageSquare className="w-4 h-4" />
+                      </button>
+                      <button onClick={()=>naviagete(`/profile/${user._id}`)} className="size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cirsor-pointer ">
+                        <Eye className="w-4 h-4" />
+                      </button>
+                </div>
+              </div>
+            ))}
+          </div>
+      </div>
     </div>
   )
 }
