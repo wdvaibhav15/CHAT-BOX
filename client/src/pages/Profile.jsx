ca5ff2cec480048319f430bc1adc1af690c9693a
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-07-08 12:47:28 +0530

    frontend done

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import Loading from '../components/Loading'
 import UserProfileInfo from '../components/UserProfileInfo'
 import PostCard from '../components/PostCard'
 import moment from 'moment'
+import ProfileModel from '../components/ProfileModel'
 
 const Profile = () => {
 
@@ -78,7 +79,7 @@ const Profile = () => {
         </div>
       </div>
       {/* Edit profile modal */}
-      {showEdit && <p >show profile edit </p>}
+      {showEdit && <ProfileModel setShowEdit={setShowEdit}/>}
     </div>
   ) : (<Loading/>)
 }
