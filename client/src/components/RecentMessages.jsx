9530bc62bb4cde87f20e2a94f7ddd163f0472298
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-07-22 18:45:29 +0530

    recent messages section done

--- client/src/components/RecentMessages.jsx
+++ client/src/components/RecentMessages.jsx
@@ -2,16 +2,51 @@ import React,{ useEffect} from 'react'
 import { dummyRecentMessagesData } from '../assets/assets';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import { useAuth, useUser } from '@clerk/clerk-react';
+import api from '../api/axios';
+import toast from 'react-hot-toast';
 
 const RecentMessages = () => {
 
     const [messages, setMessages] = React.useState([]);
+    const { user } = useUser();
+    const { getToken } = useAuth();
     const fetchRecentMessages = async() => {
-        setMessages(dummyRecentMessagesData);
+        try {
+            const token = await getToken();
+            const { data } = await api.get('/api/user/recent-messages', {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            if(data.success){
+                // froup messages by sender and get the latest message for each sender
+                const groupedMessages = data.messages.reduce((acc, message) => {
+                    const senderId = message.from_user_id._id;
+                    if (!acc[senderId] || new Date(message.createdAt) > new Date(acc[senderId].createdAt)) {
+                        acc[senderId] = message;
+                    }
+                    return acc;
+                }, {})
+                // sort messages by date
+                const sortedMessages = Object.values(groupedMessages).sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+                setMessages(sortedMessages);
+            }else{
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
     }
     useEffect(() => {
-        fetchRecentMessages();
-    },[])
+        if(user){
+            fetchRecentMessages();
+            setInterval(fetchRecentMessages, 30000);
+            return () => {
+                clearInterval();
+            }
+        }
+    },[user])
     
   return (
     <div className="bg-white max-w-xs mt-4 p-4 min-h-20 rounded-md shadow text-xs text-slate-800">
